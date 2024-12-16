c64951359400dc01e33bf0c18f2438f60092154b
Jipang2 <jspar2k70@gmail.com>
2024-12-16 11:49:46 +0900

    feat: Calculation fibonachi num with binet

--- fibo.py
+++ fibo.py
@@ -1,6 +1,12 @@
-def fibo(num):
-    if num <2:
+# def fibo(num):
+#    if num <2:
         return num
-    else:
-        return fibo(num-1) + fibo(nm-2)
+#    else:
+#        return fibo(num-1) + fibo(nm-2)
+from math import sqrt
+
+def fibo(num):
+    phi = (1 + math.sqrt(5)) / 2
+  
+    return round(pow(phi, n) / math.sqrt(5))
 print(fibo(5))
